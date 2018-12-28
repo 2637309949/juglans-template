04be2be3eee6c02f981c0bcf04e782a20c444ae1
2637309949@qq.com <2637309949@qq.com>
2018-12-28 11:06:22 +0800

    框架优化

--- src/tasks/Hello.js
+++ src/tasks/Hello.js
@@ -1,11 +1,3 @@
-/**
- * @author [Double]
- * @email [example@mail.com]
- * @create date 2018-10-11 21:08:05
- * @modify date 2018-10-11 21:08:05
- * @desc [只保留6个月内的Token, 分析Token]
-*/
-
 const defineSchedule = {
   name: 'Hello',
   spec: '*/5 * * * * *',
