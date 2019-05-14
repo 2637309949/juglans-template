73fb68597f3db37ee46186292f782d118b53b6d6
double <2637309949@qq.com>
2019-05-14 16:15:31 +0800

    add route test

--- test/app.js
+++ test/app.js
@@ -2,9 +2,7 @@ const superagent = require('supertest')
 const pApp = require('../src/app')
 
 module.exports = new Promise((resolve, reject) => {
-  console.log('supertest')
   pApp.Run(function ({ httpProxy }) {
-    console.log('httpProxy = ', httpProxy)
     resolve(superagent(httpProxy.listen()))
   })
 })
