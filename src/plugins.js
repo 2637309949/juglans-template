f7d90cdbf8717bb149bb7fedd9bdb12e274e2d2f
double <2637309949@qq.com>
2019-05-27 16:05:06 +0800

    add Captcha

--- src/plugins.js
+++ src/plugins.js
@@ -54,23 +54,21 @@ module.exports = function (app) {
   }))
 
   // Captcha Plugin
-  app.Use(
-    Captcha({
-      urlPrefix: '/captcha',
-      maxAge: 60000,
-      config: {
-        size: 4,
-        ignoreChars: '0o1i',
-        noise: 1,
-        color: true,
-        background: '#cc9966'
-      },
-      cipher: {
-        key: '5cbd5f603fd886000e3bb75e',
-        iv: '5c9dbc4c9699fa000e1e0a98'
-      }
-    })
-  )
+  app.Use(Captcha({
+    urlPrefix: '/captcha',
+    maxAge: 60000,
+    config: {
+      size: 4,
+      ignoreChars: '0o1i',
+      noise: 1,
+      color: true,
+      background: '#cc9966'
+    },
+    cipher: {
+      key: '5cbd5f603fd886000e3bb75e',
+      iv: '5c9dbc4c9699fa000e1e0a98'
+    }
+  }))
 
   // Identity Plugin
   app.Use(Identity({
