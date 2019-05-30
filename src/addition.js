1051c2a5d6a8118df8dc306b613465202a988b41
double <2637309949@qq.com>
2019-05-30 11:09:21 +0800

    调整结构

--- src/addition.js
+++ src/addition.js
@@ -3,7 +3,7 @@
  * @email [2637309949@qq.com]
  * @create date 2019-01-05 14:31:34
  * @modify date 2019-01-05 14:31:34
- * @desc [Example Instance]
+ * @desc [全局引用类]
  */
 
 const additions = require('../../juglans-addition')
--- src/juglans.js
+++ src/juglans.js
@@ -1,10 +1,1 @@
-/**
- * @author [Double]
- * @email [2637309949@qq.com]
- * @create date 2019-01-05 14:31:34
- * @modify date 2019-01-05 14:31:34
- * @desc [Example Instance]
- */
-
-// so useful, for juglan`ver dev
 module.exports = require('../../juglans')
