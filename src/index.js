cb870dad9dcf568d3b53065a8a4a221f8f421833
double <2637309949@qq.com>
2019-06-17 16:14:03 +0800

    style: RunImmediately

--- src/index.js
+++ src/index.js
@@ -3,18 +3,4 @@
 // license that can be found in the LICENSE file.
 
 const app = require('./app')
-const logger = require('./addition').logger
-const utils = require('./utils/tools')
-
-app.Run(({ httpProxy, config, router, events }) => {
-  httpProxy.listen(utils.someOrElse(config.port, 3000), err => {
-    if (!err) {
-      logger.info(`App:${config.name}`)
-      logger.info(`App:${config.NODE_ENV}`)
-      logger.info(`App:runing on Port:${config.port}`)
-      events.emit('app:events:listen:finish', 'successful')
-    } else {
-      logger.error(err)
-    }
-  })
-})
+app.RunImmediately()
