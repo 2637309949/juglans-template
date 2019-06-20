f955ad7147598711bd602f7fc83bc534e1696f2d
double <2637309949@qq.com>
2019-06-20 14:29:39 +0800

    feat: 测试内置事件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+/* eslint-disable indent */
 // Copyright (c) 2018-2020 Double.  All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
 const app = require('./app')
+const juglans = require('./juglans')
+const logger = require('./addition').logger
+
+app.Use(({ events }) => {
+    events.on(juglans.events.SYS_JUGLANS_PLUGINS_RUNIMMEDIATELY_SUCCEED, function (message) {
+        logger.info(message)
+    })
+})
 app.RunImmediately()
