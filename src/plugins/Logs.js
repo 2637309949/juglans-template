2b51e4716255195f88a5bcdb20f5184637a37b08
double <2637309949@qq.com>
2019-06-24 16:10:34 +0800

    feat: rebuild

--- src/plugins/Logs.js
+++ src/plugins/Logs.js
@@ -6,8 +6,5 @@ const Logs = require('../../../juglans-logs')
 const path = require('path')
 
 module.exports = Logs({
-  logger: {
-    path: path.join(__dirname, '../../logger')
-  }
-  // use default print format
+  path: path.join(__dirname, '../../logger')
 })
